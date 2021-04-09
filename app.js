3125cfbeb58b1dc930497234fbf23e9cc8e99334
hana <hannakorkis96@gmail.com>
2021-04-09 15:01:05 +0200

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('#searchForm');
+const reset = document.querySelector('#Reset');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config)
+    makeImages(res.data)
+    form.elements.query.value = '';
+
+})
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+
+}
+
+const removeImage = () => {
+    const images = document.querySelectorAll('img');
+    for (let img of images) {
+        img.remove();
+    }
+
+}
+
+reset.addEventListener('click', removeImage);
+
